0e0d9587ac5aa0ed4d075a8e51c313d66289a060
Justin Garrison <justin@linux.com>
2021-11-04 14:53:07 -0700

    Update eks-thw.rb

--- Formula/eks-thw.rb
+++ Formula/eks-thw.rb
@@ -2,7 +2,6 @@ class EksThw < Formula
     desc "Meta Package for EKS The Hard Way"
     version "1.0.0"
     homepage "https://github.com/rothgar/EKS-the-hard-way"
-    bottle :unneeded
     url "file:///dev/null"
     sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
