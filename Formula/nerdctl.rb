9f1f549070e32db6814a67db67cfd0b520bdac37
Justin Garrison <justinleegarrison@gmail.com>
2021-12-22 16:38:22 -0800

    Update nerdctl.rb
    bump version to 15.0

--- Formula/nerdctl.rb
+++ Formula/nerdctl.rb
@@ -2,16 +2,16 @@
 class Nerdctl < Formula
   desc "Package for nerdctl"
   homepage "https://github.com/containerd/nerdctl"
-  version "0.11.1"
+  version "0.15.0"
 
   if OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/containerd/nerdctl/releases/download/v0.11.1/nerdctl-full-0.11.1-linux-amd64.tar.gz"
+      url "https://github.com/containerd/nerdctl/releases/download/v0.15.0/nerdctl-full-0.15.0-linux-amd64.tar.gz"
       sha256 "ce7a6e119b03c3fb8ded3d46d929962fd17417bea1d5bbc07e0fce49494d8a09"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/containerd/nerdctl/releases/download/v0.11.1/nerdctl-full-0.11.1-linux-arm64.tar.gz"
+        url "https://github.com/containerd/nerdctl/releases/download/v0.15.0/nerdctl-full-0.15.0-linux-arm64.tar.gz"
         sha256 "aa85f3d9a0a44db437a04ad6e0cefa300b6ed8dee6d7f477eec9cb996efebc89"
       else
       end
